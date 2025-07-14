de289970c233c8a502c8f277e2f829171e84099e
pinge2 <katash.kirill10@gmail.com>
2025-07-14 14:36:47 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,90 @@
+import telebbot as tb
+import os
+
+
+
+bot = tb.TeleBot(os.env["BOT_TOKEN"])
+
+
+@bot.message_handler(commands=["/start"])
+def start_bot(msg):
+	bot.reply_to(msg, "Записываю пользователей в базу...\nАнтимаг пинге мой батя ;)\n\n**Все находящиеся в чате пользователи, напишите \"/reg\".")
+	users = eval(open("/list.json", "r").read())
+
+
+
+@bot.message_handler(commands=["/exit_session"])
+def save_all(msg):
+	bot.reply_to(msg, "**Сохранение...**\nГотово! До встречи ;)")
+	with open("./list.json", "w") as fd:
+		fd.write(str(users))
+	exit(0)
+
+
+
+@bot.message_handler(commands=["/reg"])
+def registry_user(msg):
+	chat_id = msg.chat.id
+	user_id = msg.reply_to_message.from_user.id
+	user_name = bot.get_chat_member(chat_id, user_id).username
+	users[user_id] = {
+	"Имя": user_name,
+	"Здоровье": 100,
+	"Сила": 1,
+	"Мана": 100,
+	"Выносливость": 1,
+	"Стойкость": 1,
+	"Время оглушения": 0,
+	"Время возрождения": 0,
+	"Суперспособность": random.randint(0, 8)
+	}
+	bot.reply_to(msg, "Пользователь записан в базу!")
+
+
+
+@bot.message_handler(commands=["*мои статы*"])
+def get_stats(msg):
+	chat_id = msg.chat.id
+	user_id = msg.reply_to_message.from_user.id
+	
+	fmt = ""
+	
+	for key in users[user_id].keys():
+		fmt += f"**{key}**: {users[user_id][key]}\n"
+	
+	bot.reply_to(msg, "**Ваши статы:**\n\n" + fmt)
+
+
+
+@bot.message_handler(commands=["*качать руки*"])
+def pushup_arms(msg):
+	chat_id = msg.chat.id
+	user_id = msg.reply_to_message.from_user.id
+	
+	users[user_id]["Сила"] += 0.5
+	users[user_id]["Мана"] -= 25
+	users[user_id]["Выносливость"] += 0.1
+	
+	fmt = f"\n\nСила удара: {users[user_id]['Сила']} (+0.5 за тренировку)"
+	fmt += f"\nТекущая мана: {users[user_id]['Мана']} (-25 за тренировку)"
+	fmt += f"\nВыносливость: {users[user_id]['Выносливость']} (+0.1 за тренировку)"
+	
+	bot.reply_to(msg, f"{users[user_id]['Имя']}, вы накачали руки и усилили свои удары!" + fmt)
+
+
+
+@bot.message_handler(commands=["*качать пресс*"])
+def pushup_arms(msg):
+	chat_id = msg.chat.id
+	user_id = msg.reply_to_message.from_user.id
+	
+	users[user_id]["Сила"] += 0.5
+	users[user_id]["Мана"] -= 25
+	users[user_id]["Выносливость"] += 0.1
+<------>
+	fmt = f"\n\nСила удара: {users[user_id]['Сила']
+	fmt += f"\nТекущая мана: {users[user_id]['Мана'
+<------>fmt += f"\nВыносливость: {users[user_id]['Вынос
+<------>
+<------>bot.reply_to(msg, f"{users[user_id]['Имя']}, вы
+
